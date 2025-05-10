e0cb06b890547c57dee8d1ef67eb4b43667bc7e3
HamzaMiraz <amirhamza11170@gmil.com>
2025-05-10 20:18:44 +0600

    2nd

---
+++ js1.js
@@ -0,0 +1,21 @@
+alert("hello naighbor");
+a=20;
+let b=20;
+let c=a+b;
+
+let student={
+    name : prompt("enter name"),
+    roll : 21224103107,
+    age : 20,
+};
+console.log(typeof student);
+console.log(typeof student.name);
+console.log(typeof student['roll']);
+console.log(student.name);
+
+let arr=['h','ajshdgj',345];
+let st='miraz';
+console.log(arr);
+for(let i=0;i<st.length;i++){
+    arr[i]=st[i];
+}
---
+++ logic.js
@@ -0,0 +1,4 @@
+alert("hallow")
+let hing=document.getElementById("hing");
+console.dir(document.body);
+console.log(document.body);
